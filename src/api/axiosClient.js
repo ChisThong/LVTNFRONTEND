9b91c976e925689bb8fd1d8e1bc5608ad648ae6d
Duy <iamsora2510@gmail.com>
2026-06-08 18:11:18 +0700

    feat: cap nhat giao dien

---
+++ src/api/axiosClient.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const axiosClient = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+// Tự động gắn Bearer token từ localStorage vào mỗi request
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosClient;
