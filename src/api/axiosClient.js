31cb8c8c18a333e7661ba78c9f5529d2b480ada8
ChisThong <nguyenchithong.209@gmail.com>
2026-08-04 02:35:02 +0700

    chữa

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://lvtnfrontend.vercel.app/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://lvtnbackend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
